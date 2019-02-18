87fdc09697e76a2aafd4088643ce2d6f88a12f45
Paul Dennis <paul_dennis@homedepot.com>
2019-02-18 17:10:16 -0500

    Further cleanup, refactoring, and commenting.

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,7 +1,6 @@
 import React from "react"
 import update from 'immutability-helper';
 var cloneDeep = require('lodash.clonedeep');
-//import Square from "./Square"
 
 
 function Square(props) {
@@ -11,58 +10,12 @@ function Square(props) {
   const blackBg = {
     backgroundColor: 'black'
   };
-  const redBg = {
-    backgroundColor: 'red'
-  };
-  const blueBg = {
-    backgroundColor: 'blue'
-  };
-  const greenBg = {
-    backgroundColor: 'green'
-  };
-  const yellowBg = {
-    backgroundColor: 'yellow'
-  };
   var style;
   if (props.value === true) {
     style = blackBg;
-  } else if (props.value === 'highlight-r') {
-    style = redBg;
-  } else if (props.value === 'highlight-b') {
-    style = blueBg;
-  } else if (props.value === 'highlight-g') {
-    style = greenBg;
-  } else if (props.value === 'highlight-y') {
-    style = yellowBg;
   } else {
     style = whiteBg;
   }
-
-  /*if (props.value === true) {
-    return (
-      <button className="square" onClick={props.onClick} style={blackBg}/>
-    );
-  } else if (props.value === 'highlight-r') {
-    return (
-      <button className="square" onClick={props.onClick} style={redBg}/>
-    );
-  } else if (props.value === 'highlight-b') {
-    return (
-      <button className="square" onClick={props.onClick} style={blueBg}/>
-    );
-  } else if (props.value === 'highlight-g') {
-    return (
-      <button className="square" onClick={props.onClick} style={greenBg}/>
-    );
-  } else if (props.value === 'highlight-y') {
-    return (
-      <button className="square" onClick={props.onClick} style={yellowBg}/>
-    );
-  } else {
-    return (
-      <button className="square" onClick={props.onClick} style={whiteBg}/>
-    );
-  }*/
 
   return (
     <button className="square" onClick={props.onClick} style={style}/>
@@ -72,7 +25,7 @@ function Square(props) {
 
 const SIZE = 50;
 
-function checkIndex (i, j) {
+function indexIsSafe (i, j) {
   return i >= 0 && j >= 0 && i <= (SIZE - 1) && j <= (SIZE - 1);
 }
 
@@ -122,55 +75,42 @@ class Board extends React.Component {
     });
   }
 
+  hasNeighborAt = (i, j) => {
+    const board = this.state.squares.slice();
+    if (indexIsSafe(i, j)) {
+      if (board[i][j]) {
+        return 1;
+      }
+    }
+    return 0;
+  };
+
   countLiveNeighbors = (i, j) => {
-      const board = this.state.squares.slice();
       var neighborCount = 0;
-      if (checkIndex(i-1, j-1)) {
-        if (board[i-1][j-1]) {
-          neighborCount++;
-        }
-      }
-      if (checkIndex(i-1, j)) {
-        if (board[i-1][j]) {
-          neighborCount++;
-        }
-      }
-      if (checkIndex(i-1, j+1)) {
-        if (board[i-1][j+1]) {
-          neighborCount++;
-        }
-      }
-      if (checkIndex(i, j - 1)) {
-        if (board[i][j-1]) {
-          neighborCount++;
-        }
-      }
-      if (checkIndex(i, j+1)) {
-        if (board[i][j+1]) {
-          neighborCount++;
-        }
-      }
-      if (checkIndex(i+1, j-1)) {
-        if (board[i+1][j-1]) {
-          neighborCount++;
-        }
-      }
-      if (checkIndex(i+1, j)) {
-        if (board[i+1][j]) {
-          neighborCount++;
-        }
-      }
-      if (checkIndex(i+1, j+1)) {
-        if (board[i+1][j+1]) {
-          neighborCount++;
-        }
-      }
+
+      //Check all 8 adjacent neighbors:
+      /**
+      (i-1, j-1)   (i-1, j)    (i-1, j+1)
+      (i, j-1)      (me)       (i, j+1)
+      (i+1, j-1)   (i+1, j)    (i+1, j+1)
+      */
+      neighborCount += this.hasNeighborAt(i-1, j-1);
+      neighborCount += this.hasNeighborAt(i-1, j);
+      neighborCount += this.hasNeighborAt(i-1, j+1);
+      neighborCount += this.hasNeighborAt(i, j-1);
+      neighborCount += this.hasNeighborAt(i, j+1);
+      neighborCount += this.hasNeighborAt(i+1, j-1);
+      neighborCount += this.hasNeighborAt(i+1, j);
+      neighborCount += this.hasNeighborAt(i+1, j+1);
+
       return neighborCount;
   }
 
+  //On each tick we have to iterate over each cell and decide
+  //if it lives or dies based on the game rules.
   doTick = () => {
-    const newSquares = cloneDeep(this.state.squares);
-    const origSquares = cloneDeep(this.state.squares);
+    const newSquares = cloneDeep(this.state.squares); //Copy for new data
+    const origSquares = cloneDeep(this.state.squares); //Keep a clean copy
     for (var i = 0; i < SIZE; i++) {
       for (var j = 0; j < SIZE; j++) {
         newSquares[i][j] = evaluateCell(origSquares[i][j], this.countLiveNeighbors(i, j, origSquares));
@@ -183,6 +123,7 @@ class Board extends React.Component {
   }
   render () {
     var rows = [];
+    //Create a two-dimensional array of Squares of size SIZE
     for (var i = 0; i < SIZE; i++) {
 
       var squares = [];
