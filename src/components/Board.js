ebac7121d73a0377118b7836129c814c1cfb9e0d
Paul Dennis <paul_dennis@homedepot.com>
2019-02-18 16:18:41 -0500

    Made size dynamic; general cleanip

--- src/components/Board.js
+++ src/components/Board.js
@@ -70,8 +70,10 @@ function Square(props) {
 
 }
 
+const SIZE = 50;
+
 function checkIndex (i, j) {
-  return i >= 0 && j >= 0 && i <= 9 && j <= 9;
+  return i >= 0 && j >= 0 && i <= (SIZE - 1) && j <= (SIZE - 1);
 }
 
 function evaluateCell (isAlive, livingNeighbors) {
@@ -86,15 +88,10 @@ class Board extends React.Component {
 
   constructor(props) {
     super(props);
-    var array = new Array(10);
-    for (var i = 0; i < 10; i++) {
-      array[i] = new Array(10).fill(false);
+    var array = new Array(SIZE);
+    for (var i = 0; i < SIZE; i++) {
+      array[i] = new Array(SIZE).fill(false);
     }
-    array[0][1] = true;
-    array[1][1] = true;
-    //array[0][0] = true;
-    //array[1][0] = true;
-    array[2][1] = true;
     this.state = {
       squares: array
     };
@@ -109,29 +106,6 @@ class Board extends React.Component {
         onClick={() => this.handleClick(i, j)}
       />
     );
-  }
-
-  highlightClick = (i, j) => {
-    console.log("highlighting Click at " + i + ", " + j);
-
-    const squares = this.state.squares.slice();
-
-    squares[i-1][j-1] = 'highlight-r';
-    squares[i-1][j]   = 'highlight-g';
-    squares[i-1][j+1] = 'highlight-b';
-
-    squares[i][j-1]   = 'highlight-y';
-    squares[i][j] = true;
-    squares[i][j+1]   = 'highlight-y';
-
-    squares[i+1][j-1] = 'highlight-r';
-    squares[i+1][j]   = 'highlight-g';
-    squares[i+1][j+1] = 'highlight-b';
-
-    /*
-    this.setState({
-      squares: squares
-    });*/
   }
 
   handleClick= (i, j) => {
@@ -194,80 +168,11 @@ class Board extends React.Component {
       return neighborCount;
   }
 
-  countLiveNeighborsAndLog = (i, j) => {
-    const board = this.state.squares.slice();
-    console.log(i + ", " + j);
-    if (board[i][j]) {
-      console.log("(Alive)");
-    } else {
-      console.log("(Dead)");
-    }
-    var neighborCount = 0;
-    if (checkIndex(i-1, j-1)) {
-      console.log("\tChecking " + (i-1) + ", " + (j-1))
-      if (board[i-1][j-1]) {
-        neighborCount++;
-        console.log("\tFound live neighbor at " + (i-1) + ", " + (j-1));
-      }
-    }
-    if (checkIndex(i-1, j)) {
-      console.log("\tChecking " + (i-1) + ", " + (j))
-      if (board[i-1][j]) {
-        neighborCount++;
-        console.log("\tFound live neighbor at " + (i-1) + ", " + (j));
-      }
-    }
-    if (checkIndex(i-1, j+1)) {
-      console.log("\tChecking " + (i-1) + ", " + (j+1));
-      if (board[i-1][j+1]) {
-        neighborCount++;
-        console.log("\tFound live neighbor at " + (i-1) + ", " + (j+1));
-      }
-    }
-    if (checkIndex(i, j - 1)) {
-      console.log("\tChecking " + (i) + ", " + (j-1));
-      if (board[i][j-1]) {
-        neighborCount++;
-        console.log("\tFound live neighbor at " + (i) + ", " + (j-1));
-      }
-    }
-    if (checkIndex(i, j+1)) {
-      console.log("\tChecking " + (i) + ", " + (j+1));
-      if (board[i][j+1]) {
-        neighborCount++;
-        console.log("\tFound live neighbor at " + (i) + ", " + (j+1));
-      }
-    }
-    if (checkIndex(i+1, j-1)) {
-      console.log("\tChecking " + (i+1) + ", " + (j-1))
-      if (board[i+1][j-1]) {
-        neighborCount++;
-        console.log("\tFound live neighbor at " + (i+1) + ", " + (j-1));
-      }
-    }
-    if (checkIndex(i+1, j)) {
-      console.log("\tChecking " + (i+1) + ", " + (j));
-      if (board[i+1][j]) {
-        neighborCount++;
-        console.log("\tFound live neighbor at " + (i+1) + ", " + (j));
-      }
-    }
-    if (checkIndex(i+1, j+1)) {
-      console.log("\tChecking " + (i+1) + ", " + (j+1));
-      if (board[i+1][j+1]) {
-        neighborCount++;
-        console.log("\tFound live neighbor at " + (i+1) + ", " + (j+1));
-      }
-    }
-    //console.log("countLiveNeighbors(" + i + ", " + j + ") returning: " + neighborCount);
-    return neighborCount;
-  }
-
   doTick = () => {
     const newSquares = cloneDeep(this.state.squares);
     const origSquares = cloneDeep(this.state.squares);
-    for (var i = 0; i < 10; i++) {
-      for (var j = 0; j < 10; j++) {
+    for (var i = 0; i < SIZE; i++) {
+      for (var j = 0; j < SIZE; j++) {
         newSquares[i][j] = evaluateCell(origSquares[i][j], this.countLiveNeighbors(i, j, origSquares));
       }
     }
@@ -277,129 +182,20 @@ class Board extends React.Component {
     });
   }
   render () {
+    var rows = [];
+    for (var i = 0; i < SIZE; i++) {
+
+      var squares = [];
+      for (var j = 0; j < SIZE; j++) {
+        squares.push(this.renderSquare(i, j));
+      }
+      var col = <div className="board-row">{squares}</div>;
+      rows.push(col);
+    }
     return (
       <div>
         <div>
-          <div className="board-row">
-            {this.renderSquare(0, 0)}
-            {this.renderSquare(0, 1)}
-            {this.renderSquare(0, 2)}
-            {this.renderSquare(0, 3)}
-            {this.renderSquare(0, 4)}
-            {this.renderSquare(0, 5)}
-            {this.renderSquare(0, 6)}
-            {this.renderSquare(0, 7)}
-            {this.renderSquare(0, 8)}
-            {this.renderSquare(0, 9)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(1, 0)}
-            {this.renderSquare(1, 1)}
-            {this.renderSquare(1, 2)}
-            {this.renderSquare(1, 3)}
-            {this.renderSquare(1, 4)}
-            {this.renderSquare(1, 5)}
-            {this.renderSquare(1, 6)}
-            {this.renderSquare(1, 7)}
-            {this.renderSquare(1, 8)}
-            {this.renderSquare(1, 9)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(2, 0)}
-            {this.renderSquare(2, 1)}
-            {this.renderSquare(2, 2)}
-            {this.renderSquare(2, 3)}
-            {this.renderSquare(2, 4)}
-            {this.renderSquare(2, 5)}
-            {this.renderSquare(2, 6)}
-            {this.renderSquare(2, 7)}
-            {this.renderSquare(2, 8)}
-            {this.renderSquare(2, 9)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3, 0)}
-            {this.renderSquare(3, 1)}
-            {this.renderSquare(3, 2)}
-            {this.renderSquare(3, 3)}
-            {this.renderSquare(3, 4)}
-            {this.renderSquare(3, 5)}
-            {this.renderSquare(3, 6)}
-            {this.renderSquare(3, 7)}
-            {this.renderSquare(3, 8)}
-            {this.renderSquare(3, 9)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(4, 0)}
-            {this.renderSquare(4, 1)}
-            {this.renderSquare(4, 2)}
-            {this.renderSquare(4, 3)}
-            {this.renderSquare(4, 4)}
-            {this.renderSquare(4, 5)}
-            {this.renderSquare(4, 6)}
-            {this.renderSquare(4, 7)}
-            {this.renderSquare(4, 8)}
-            {this.renderSquare(4, 9)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(5, 0)}
-            {this.renderSquare(5, 1)}
-            {this.renderSquare(5, 2)}
-            {this.renderSquare(5, 3)}
-            {this.renderSquare(5, 4)}
-            {this.renderSquare(5, 5)}
-            {this.renderSquare(5, 6)}
-            {this.renderSquare(5, 7)}
-            {this.renderSquare(5, 8)}
-            {this.renderSquare(5, 9)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6, 0)}
-            {this.renderSquare(6, 1)}
-            {this.renderSquare(6, 2)}
-            {this.renderSquare(6, 3)}
-            {this.renderSquare(6, 4)}
-            {this.renderSquare(6, 5)}
-            {this.renderSquare(6, 6)}
-            {this.renderSquare(6, 7)}
-            {this.renderSquare(6, 8)}
-            {this.renderSquare(6, 9)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(7, 0)}
-            {this.renderSquare(7, 1)}
-            {this.renderSquare(7, 2)}
-            {this.renderSquare(7, 3)}
-            {this.renderSquare(7, 4)}
-            {this.renderSquare(7, 5)}
-            {this.renderSquare(7, 6)}
-            {this.renderSquare(7, 7)}
-            {this.renderSquare(7, 8)}
-            {this.renderSquare(7, 9)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(8, 0)}
-            {this.renderSquare(8, 1)}
-            {this.renderSquare(8, 2)}
-            {this.renderSquare(8, 3)}
-            {this.renderSquare(8, 4)}
-            {this.renderSquare(8, 5)}
-            {this.renderSquare(8, 6)}
-            {this.renderSquare(8, 7)}
-            {this.renderSquare(8, 8)}
-            {this.renderSquare(8, 9)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(9, 0)}
-            {this.renderSquare(9, 1)}
-            {this.renderSquare(9, 2)}
-            {this.renderSquare(9, 3)}
-            {this.renderSquare(9, 4)}
-            {this.renderSquare(9, 5)}
-            {this.renderSquare(9, 6)}
-            {this.renderSquare(9, 7)}
-            {this.renderSquare(9, 8)}
-            {this.renderSquare(9, 9)}
-          </div>
+          {rows}
         </div>
         <button onClick={this.doTick}>
         Advance
